1b20fedc18097cae422b9fed3774ec204159eb08
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-06 13:33:55 +0200

    Create us.js

---
+++ Source/us.js
@@ -0,0 +1,34 @@
+var source = {
+ /* networks */
+ 'ABC (East)' : '',
+ 'CBS (East)' : '',
+ 'NBC (East)' : '',
+ 'FOX (East)' : '',
+ 'CW (East)' : '',
+ 'PBS (East)' : '//status.tehw00t.net/miniProxy.php/https://wnetdt.lls.pbs.org/out/v1/0456457548354b88b32fc437e4e7ee01/index.m3u8',
+ /* news */
+ 'ABC News': '//content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8',
+ 'CBS News': '//cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8',
+ 'NBC News': '//nbcnews2.akamaized.net/hls/live/723426/NBCNewsPlaymaker24x7Linear99a3a827-ua/master.m3u8',
+ 'CNN': '//cnnpro.newscdn.org/NF54vHKmvXlMhQ5xw4QqQQ==,1646612404/1384240915/rewind-48400.m3u8',
+ 'Fox News': '//1312990316.rsc.cdn77.org/m39AwZqPrWmNsyHKkqbiWg==,1646612404/1312990316/rewind-24400.m3u8',
+ 'MSNBC': '//1229472784.rsc.cdn77.org/FrqFUvV90YPCJQbjM6Jueg==,1646615203/1229472784/rewind-28000.m3u8',
+ /* small */
+ 'HLN': '//turnerlive.warnermediacdn.com/hls/live/586496/cnngo/hln/VIDEO_0_3564000.m3u8',
+ 'NewsNation': '//dcs-live.mp.lura.live/server/play/p0d3eNBh3XOxUQP7/manifest.m3u8',
+ 'Newsmax': '//nmxlive.akamaized.net/hls/live/529965/Live_1/index.m3u8',
+ 'OANN': '//oneamericanews-roku-us.amagi.tv/playlist.m3u8',
+ 'Live FOX': '//fox-foxnewsnow-samsungus.amagi.tv/playlist.m3u8',
+ 'Cheddar': '//live.chdrstatic.com/cbn/index.m3u8',
+ /* politics */
+ 'C-SPAN 1': '',
+ 'C-SPAN 2': '',
+ 'C-SPAN 3': '',
+  /* business */
+ 'CNBC': '',
+ 'Fox Business': '',
+ 'Bloomberg': '//bloomberg-bloomberg-1-pt.samsung.wurl.com/manifest/playlist.m3u8',
+ /* weather */
+ 'Weather Channel': '',
+ 'Fox Weather': '',
+};
