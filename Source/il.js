7183038c40a6271328e34c1e21cf39c75a4a2c1f
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-14 20:22:18 +0200

    Update il.js

--- Source/il.js
+++ Source/il.js
@@ -9,26 +9,26 @@ var channels = {
  /* קטנים */
   CH21: [{name:'ערוץ הקניות', link:'//shoppingil-rewriter.vidnt.com/index.m3u8'}],
   CH24: [{name:'ערוץ 24', link:'//keshethlslive-lh.akamaihd.net/i/24live_1@195271/index_1400_av-b.m3u8'}],
+  CH26: [{name:'האח הגדול', link:'//'}],
   CH80: [{name:'חינוכית', link:'//kanlivep2event-i.akamaihd.net/hls/live/747600/747600/master.m3u8'}],
   CH96: [{name:'קבלה לעם', link:'//edge2.il.kab.tv/live/tv66-heb-high/playlist.m3u8'}],
   CH97: [{name:'הידברוץ', link:'//stream72.shidur.net/htvlive2/_definst_/smil:live2.smil/playlist.m3u8'}],
   CH99: [{name:'כנסת', link:'//contact.gostreaming.tv/Knesset/myStream/playlist.m3u8'}],
-
- /* אתרים */
-  Ynet: [{name:'Ynet', link:'//ynet-lh.akamaihd.net/i/ynet_1@123293/master.m3u8'}],
-  Walla: [{name:'Walla', link:'//live.wcdn.co.il/news/prog_index.m3u8'}],
-  N12: [{name:'N12', link:'//keshethlslive-lh.akamaihd.net/i/c2nsec_1@195268/master.m3u8'}],
 
  /* שפות */
   CH09: [{name:'ערוץ 9', link:'//50.7.231.220:8081/185/index.m3u8?wmsAuthSign=okad'}],
   CH30: [{name:'הלא TV', link:'//gstream4.panet.co.il/edge/halaTV/playlist.m3u8'}],
   CH33: [{name:'מכאן', link:'//kanlivep2event-i.akamaihd.net/hls/live/747613/747613/master.m3u8'}],
   CH34: [{name:'i24News', link:'//'}],
+ 
+ /* אתרים */
+  Ynet: [{name:'Ynet', link:'//ynet-lh.akamaihd.net/i/ynet_1@123293/master.m3u8'}],
+  Walla: [{name:'Walla', link:'//live.wcdn.co.il/news/prog_index.m3u8'}],
+  N12: [{name:'N12', link:'//keshethlslive-lh.akamaihd.net/i/c2nsec_1@195268/master.m3u8'}],
 
  /* כתוביות */
   CC11: [{name:'כאן 11', link:'//kanlivep2event-i.akamaihd.net/hls/live/747610/747610/master.m3u8'}],
   CC12: [{name:'קשת 12', link:'//keshethlslive-lh.akamaihd.net/i/CH12CC_1@732426/index_2200_av.m3u8'}],
   CC13: [{name:'רשת 13', link:'//d198ztbnlup2iq.cloudfront.net/out/v1/2d9050c90fb94df8b78d1d98306a1a65/index.m3u8'}],
-  CC14: [{name:'עכשיו 14', link:'//splittv.ch20-cdnwiz.com/hls/live.m3u8'}],
  
 };
