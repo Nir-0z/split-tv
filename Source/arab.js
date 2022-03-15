273854934289928003d5763a8dbdaa2c04ef2edc
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-15 15:59:31 +0200

    Create arab.js

---
+++ Source/arab.js
@@ -0,0 +1,22 @@
+var source = {
+
+ /* World */
+
+  /* Aabia */
+  Jazeera: [{name:'الجزيرة', link:'//live-hls-web-aja.getaj.net/AJA/index.m3u8'}],
+  Arabiya: [{name:'العربية', link:'//live.alarabiya.net/alarabiapublish/alarabiya.smil/playlist.m3u8'}],
+  Hadath: [{name:'الحدث', link:'//av.alarabiya.net/alarabiapublish/alhadath.smil/playlist.m3u8'}],
+
+ /* USA */
+  Alhurra: [{name:'الحرة', link:'//mbningestworld-i.akamaihd.net/hls/live/586122/worldsafe/master.m3u8'}],
+
+ /* Palestine */
+  Musawa: [{name:'مساواة', link:'//pbc.furrera.ps/musawahd/index.m3u8'}],
+  Quds: [{name:'قناة القدس', link:'//185.5.222.146:8081/live/habbq18/playlist.m3u8'}],
+  Aqsa: [{name:'قناة الأقصى', link:'//dacastmmd.mmdlive.lldns.net/dacastmmd/91fd19e6da5d4acd833174df1dd9344d/manifest.m3u8'}],
+
+ /* Lebanon */
+  Mayadeen: [{name:'الميادين', link:'//lmdstrm.cdn.octivid.com/mayadeen-live/smil:mayadeen.smil/playlist.m3u8'}],
+  Manar: [{name:'المنار', link:'//manar.live/iptv/index.m3u8'}],
+
+};
