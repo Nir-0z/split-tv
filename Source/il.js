4a59a2e5139415d11077b7c0bb98282a911155bc
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-15 10:34:07 +0200

    Update il.js

--- Source/il.js
+++ Source/il.js
@@ -1,10 +1,8 @@
-<script>
  $(function() {
 $.getJSON("//mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/i/CH12LIVE_HIGH/index.m3u8?b=200-2500&rv=AKAMAI", function(json12){
 source['CH12'][0].link = 'https://keshethlslive-i.akamaihd.net/hls/live/512033/CH2LIVE_HIGH/index.m3u8?' + json12.tickets[0].ticket;
 });
 });
-</script>
 
 var source = {
 
