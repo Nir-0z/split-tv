7138ddbec519993611caeb73e3eaa596c5cdf013
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-15 16:26:44 +0200

    Create uk.js

---
+++ Source/uk.js
@@ -0,0 +1,16 @@
+var source = {
+
+ /* Broadcast */
+  BBC1: [{name:'BBC One', link:'//cdnuk001.broadcastcdn.net/KUK-BBC1-2022/index.m3u8'}],
+  BBC2: [{name:'BBC Two', link:'//cdnuk001.broadcastcdn.net/KUK-BBC2/index.m3u8'}],
+  ITV1: [{name:'ITV', link:'//cdnuk001.broadcastcdn.net/KUK-ITV-107/index.m3u8'}],
+  ITV2: [{name:'ITV 2', link:'//cdnuk001.broadcastcdn.net/KUK-ITV2/index.m3u8'}],
+  CH4: [{name:'Channel 4', link:'//cdnuk001.broadcastcdn.net/KUK-CHANNEL4/index.m3u8'}],
+  CH5: [{name:'Channel 5', link:'//cdnuk001.broadcastcdn.net/KUK-FIVE/index.m3u8'}],
+
+ /* News */
+  BBCNews: [{name:'BBC News', link:'//cdnuk001.broadcastcdn.net/KUK-BBCNEWSHD/index.m3u8'}],
+  SkyNews: [{name:'Sky News', link:'//siloh.pluto.tv/lilo/production/SkyNews/master.m3u8'}],
+  GBNews: [{name:'GB News', link:'//cdnuk001.broadcastcdn.net/KUK-GBNEWS/index.m3u8'}],
+
+};
