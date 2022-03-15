134ddb10a9fc0170b1fe2eaeff3c27889321d4b9
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-15 14:20:25 +0200

    Update world.js

--- Source/world.js
+++ Source/world.js
@@ -1,1 +1,26 @@
+var source = {
 
+ /* Global */
+  Reuters: [{name:'Reuters', link:'//euters-reutersnow-1.plex.wurl.com/manifest/playlist.m3u8'}],
+
+ /* America */
+  CNN: [{name:'CNNi', link:'//cnn-cnninternational-1-eu.rakuten.wurl.com/manifest/playlist.m3u8'}],
+  Global: [{name:'Global News', link:'//dai2.xumo.com/amagi_hls_data_xumo1212A-foxweather-xumo/CDN/master.m3u8'}],
+
+ /* Europe */
+  Euronews: [{name:'Euronews', link:'//rakuten-euronews-1-ie.samsung.wurl.com/manifest/playlist.m3u8'}],
+  BBC: [{name:'BBC World', link:'//livecdn.fptplay.net/sdb/bbcnew_hls.smil/master.m3u8'}],
+  Frane: [{name:'France 24', link:'//cdn.klowdtv.net/803B48A/n1.klowdtv.net/live2/france24_720p/chunks.m3u8'}],
+  DW: [{name:'DW', link:'//dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/index.m3u8'}],
+  RT: [{name:'RT', link:'//rt-glb.rttv.com/live/rtnews/playlist.m3u8'}],
+
+ /* Middle East */
+  AlJazeera: [{name:'Al Jazeera', link:'//live-hls-web-aje.getaj.net/AJE/master.m3u8'}],
+  TRT: [{name:'TRT', link:'//tv-trtworld.live.trt.com.tr/master.m3u8'}],
+  Press: [{name:'Press TV', link:'//live.presstv.ir/liveprs/smil:liveprs/playlist.m3u8'}],
+
+ /* Asia */
+  NHK: [{name:'NHK', link:'//nhkworld.webcdn.stream.ne.jp/www11/nhkworld-tv/global/2003458/live.m3u8'}],
+  CGTN: [{name:'CGTN', link:'//news.cgtn.com/resource/live/english/cgtn-news.m3u8'}],
+
+};
