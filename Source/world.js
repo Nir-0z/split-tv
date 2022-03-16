204a6669e6f77f1799ee4b4e1dcef5ce59529def
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-03-16 22:10:04 +0200

    Update world.js

--- Source/world.js
+++ Source/world.js
@@ -4,13 +4,14 @@ var source = {
   Reuters: [{name:'Reuters', link:'//euters-reutersnow-1.plex.wurl.com/manifest/playlist.m3u8'}],
 
  /* America */
-  CNN: [{name:'CNNi', link:'//cnn-cnninternational-1-eu.rakuten.wurl.com/manifest/playlist.m3u8'}],
+  CNNi: [{name:'CNNi', link:'//cnn-cnninternational-1-eu.rakuten.wurl.com/manifest/playlist.m3u8'}],
   Global: [{name:'Global News', link:'//dai.google.com/linear/hls/pa/event/cQOVg6jKSvusQAfYpTrUoQ/stream/29bd32bb-a620-412c-9a68-e35c8a4fb332:BRU/master.m3u8'}],
 
  /* Europe */
   Euronews: [{name:'Euronews', link:'//rakuten-euronews-1-ie.samsung.wurl.com/manifest/playlist.m3u8'}],
   BBC: [{name:'BBC World', link:'//livecdn.fptplay.net/sdb/bbcnew_hls.smil/master.m3u8'}],
-  Frane: [{name:'France 24', link:'//cdn.klowdtv.net/803B48A/n1.klowdtv.net/live2/france24_720p/chunks.m3u8'}],
+  SKY: [{name:'Sky News', link:'//siloh.pluto.tv/lilo/production/SkyNews/master.m3u8'}],
+  France: [{name:'France 24', link:'//cdn.klowdtv.net/803B48A/n1.klowdtv.net/live2/france24_720p/chunks.m3u8'}],
   DW: [{name:'DW', link:'//dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/index.m3u8'}],
   RT: [{name:'RT', link:'//rt-glb.rttv.com/live/rtnews/playlist.m3u8'}],
 
