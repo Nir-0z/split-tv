de167defa6eeae889cb2ca24764badbeee035d68
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-04-25 14:14:58 +0300

    Update france.js

--- Source/france.js
+++ Source/france.js
@@ -4,8 +4,6 @@ var source = {
   TF1: [{name:'TF1', link:'//s4.tntendirect.com/tf1/live/playlist.m3u8'}],
   France2: [{name:'France 2', link:'//s4.tntendirect.com/france2/live/playlist.m3u8'}],
   France3: [{name:'France 3', link:'//s4.tntendirect.com/france3/live/playlist.m3u8'}],
-  France4: [{name:'France 4', link:'//s4.tntendirect.com/france4/live/playlist.m3u8'}],
-  France5: [{name:'France 5', link:'//s4.tntendirect.com/france5/live/playlist.m3u8'}],
   M6: [{name:'M6', link:'//s2.tntendirect.com/m6/live/playlist.m3u8'}],
   TMC: [{name:'TMC', link:'//s2.tntendirect.com/tmc/live/playlist.m3u8'}],
   C8: [{name:'C8', link:'//s4.tntendirect.com/d8/live/playlist.m3u8'}],
@@ -17,5 +15,6 @@ var source = {
   LCI: [{name:'LCI', link:'//sv0.data-stream.top/hls/lci.m3u8'}],
   CNews: [{name:'Cnews', link:'//x3b68jn'}],
   FranceInfo: [{name:'France Info', link:'//x4rdeu6'}],
+  France24EN: [{name:'France 24 EN', link:'//cdn.klowdtv.net/803B48A/n1.klowdtv.net/live2/france24_720p/chunks.m3u8'}],
 
 };
