115b2565395ce3fa02432983db0efe55cf97508e
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-04-25 14:10:56 +0300

    Create france.js

---
+++ Source/france.js
@@ -0,0 +1,21 @@
+var source = {
+
+ /* Broadcast */
+  TF1: [{name:'TF1', link:'//s4.tntendirect.com/tf1/live/playlist.m3u8'}],
+  France2: [{name:'France 2', link:'//s4.tntendirect.com/france2/live/playlist.m3u8'}],
+  France3: [{name:'France 3', link:'//s4.tntendirect.com/france3/live/playlist.m3u8'}],
+  France4: [{name:'France 4', link:'//s4.tntendirect.com/france4/live/playlist.m3u8'}],
+  France5: [{name:'France 5', link:'//s4.tntendirect.com/france5/live/playlist.m3u8'}],
+  M6: [{name:'M6', link:'//s2.tntendirect.com/m6/live/playlist.m3u8'}],
+  TMC: [{name:'TMC', link:'//s2.tntendirect.com/tmc/live/playlist.m3u8'}],
+  C8: [{name:'C8', link:'//s4.tntendirect.com/d8/live/playlist.m3u8'}],
+  Arte: [{name:'Arte', link:'//s2.tntendirect.com/tmc/live/playlist.m3u8'}],
+  W9: [{name:'W9', link:'//s2.tntendirect.com/w9/live/playlist.m3u8'}],  
+
+ /* News */
+  BFMTV: [{name:'BFM TV', link:'//s2.tntendirect.com/bmftv/live/playlist.m3u8'}],
+  LCI: [{name:'LCI', link:'//sv0.data-stream.top/hls/lci.m3u8'}],
+  CNews: [{name:'Cnews', link:'//x3b68jn'}],
+  FranceInfo: [{name:'France Info', link:'//x4rdeu6'}],
+
+};
