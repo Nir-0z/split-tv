87c5aa6e4478fecef8e07d7109817d60e5897760
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-05-08 21:46:32 +0300

    Create germany.js

---
+++ Source/germany.js
@@ -0,0 +1,16 @@
+var source = {
+
+ /* Broadcast */
+  ZDF: [{name:'ZDF', link:'//wu.wukongtv.ga/ZDF/video.m3u8'}],
+  ARD: [{name:'ARD', link:'//mcdn.daserste.de/daserste/de/master.m3u8'}],
+  RTL: [{name:'ARD', link:'//wu.wukongtv.ga/RTL/video.m3u8'}],
+  SAT1: [{name:'SAT1', link:'//wu.wukongtv.ga/SAT1/video.m3u8'}],
+
+ /* News */
+  NTV: [{name:'N-tv', link:'//wu.wukongtv.ga/NTV/video.m3u8'}],
+  Phoenix: [{name:'Phoenix', link:'//zdf-hls-19.akamaized.net/hls/live/2016502/de/high/master.m3u8'}],
+  WELT: [{name:'WELT', link:'//wu.wukongtv.ga/N24/video.m3u8'}],
+  Tagesschau24: [{name:'WELT', link:'//tagesschau.akamaized.net/hls/live/2020117/tagesschau/tagesschau_3/master.m3u8'}],
+  WELT: [{name:'WELT', link:'//bild.personalstream.tv/v1/master.m3u8'}],
+
+};
