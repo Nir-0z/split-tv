46948920d12bba24584b6968267efa0bda18228f
Nir Oz <47395913+NirOzFiles@users.noreply.github.com>
2022-06-07 01:10:42 +0300

    Update uk.js

--- Source/uk.js
+++ Source/uk.js
@@ -1,17 +1,17 @@
 var source = {
 
  /* Broadcast */
-  BBC1: [{name:'BBC One', link:'//cdnuk001.broadcastcdn.net/KUK-BBC1-2022/index.m3u8'}],
-  BBC2: [{name:'BBC Two', link:'//cdnuk001.broadcastcdn.net/KUK-BBC2/index.m3u8'}],
-  ITV1: [{name:'ITV', link:'//cdnuk001.broadcastcdn.net/KUK-ITV-107/index.m3u8'}],
-  ITV2: [{name:'ITV 2', link:'//cdnuk001.broadcastcdn.net/KUK-ITV2/index.m3u8'}],
-  CH4: [{name:'Channel 4', link:'//cdnuk001.broadcastcdn.net/KUK-CHANNEL4/index.m3u8'}],
-  CH5: [{name:'Channel 5', link:'//cdnuk001.broadcastcdn.net/KUK-FIVE/index.m3u8'}],
+  BBC1: [{name:'BBC One', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-BBC1-2022/index.m3u8'}],
+  BBC2: [{name:'BBC Two', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-BBC2/index.m3u8'}],
+  ITV1: [{name:'ITV', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-ITV-107/index.m3u8'}],
+  ITV2: [{name:'ITV 2', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-ITV2/index.m3u8'}],
+  CH4: [{name:'Channel 4', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-CHANNEL4/index.m3u8'}],
+  CH5: [{name:'Channel 5', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-FIVE/index.m3u8'}],
 
  /* News */
-  BBCNews: [{name:'BBC News', link:'//cdnuk001.broadcastcdn.net/KUK-BBCNEWSHD/index.m3u8'}],
+  BBCNews: [{name:'BBC News', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://cdnuk001.broadcastcdn.net/KUK-BBCNEWSHD/index.m3u8'}],
   SkyNews: [{name:'Sky News', link:'//siloh.pluto.tv/lilo/production/SkyNews/master.m3u8'}],
-  GBNews: [{name:'GB News', link:'//cdnuk001.broadcastcdn.net/KUK-GBNEWS/index.m3u8'}],
+  GBNews: [{name:'GB News', link:'//ersupport.com/plugins/QuickWebProxy/miniProxy.php/https://live-gbnews.simplestreamcdn.com/gbnews/gbnews/bitrate1.isml/live.m3u8'}],
   TalkTV: [{name:'Talk TV', link:'//cdn-ue1-prod.tsv2.amagi.tv/linear/amg00738-newsuk-talkradiotv-ono/playlist.m3u8'}],
 
 };
